2dd2bc7c891c3ff4ef8a646e5fc72f2f6c29828f
Faslh Generator <generator@faslh.com>
2023-05-24 16:27:39 +0000

    local

---
+++ tmp/d2c484c6-7811-487c-b60d-7edade33973d/src/core/operation.ts
@@ -0,0 +1,35 @@
+import { randomUUID as v4 } from 'crypto';
+import { EventEmitter, once } from 'events';
+
+const end = Symbol('A symbol to mark the end of the operation');
+const messageBus = new EventEmitter({ captureRejections: true });
+
+export async function handleOperation(operation: string, input: any) {
+  const operationId = v4();
+  const message = {
+    ...input,
+    [end]: (output: any, error: any) => {
+      if (error) {
+        console.error(`Error processing ${operation} operation `, error);
+        throw error;
+      }
+      messageBus.emit(operationId, output);
+    },
+  };
+  messageBus.emit(operation, message);
+  const output = await once(messageBus, operationId);
+  messageBus.removeAllListeners(operationId);
+  return output;
+}
+
+export function listen(operation: string, callback: (...args: any) => any) {
+  messageBus.on(operation, async (message) => {
+    const { [end]: endFn, ...input } = message;
+    try {
+      const output = await callback(input);
+      endFn(output, null);
+    } catch (error) {
+      endFn(null, error);
+    }
+  });
+}
